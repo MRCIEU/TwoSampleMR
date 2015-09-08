28fa3dc767626c15d38257067f7d6b81e9d4f4ad
Gibran Hemani <explodecomputer@gmail.com>
2015-09-08 20:56:50 +0100

    updated but can't make many tests because epi franklin is rammed

--- R/mysql.R
+++ R/mysql.R
@@ -1,13 +1,55 @@
-
-extract_outcome_data <- function(exposure_dat, outcomes, user, password, dbname, host)
+#' Get list of studies with available GWAS summary statistics
+#'
+#' @param user="mruser" User name
+#' @param password="TMG_F1WnTL" Password
+#' @param dbname="mrbase" Database
+#' @param host="epi-franklin.epi.bris.ac.uk" Host
+#' @export
+#' @return Dataframe of study details
+available_outcomes <- function(user="mruser", password="TMG_F1WnTL", dbname="mrbase", host="epi-franklin.epi.bris.ac.uk")
 {
-	# This function will 
-	# connect to the database
-	# run a query to find all SNPs for all specified outcomes 
-	# return a dataframe of each SNP in each outcome
+	mydb <- dbConnect(MySQL(), user=user, password=password, dbname=dbname, host=host)
+	query <- "SELECT * from study;"
+	out <- dbSendQuery(mydb, query)
+	d <- fetch(out, n=-1)
+	return(d)
+}
 
-	# outcome_dat column names:
-	# SNP, beta.outcome, se.outcome, eaf.outcome, effect_allele.outcome, other_allele.outcome, outcome
 
+#' Extract SNP effects from GWAS summary statistics
+#'
+#' 
+#' @param exposure_dat Output from \code{read_exposure_data}
+#' @param outcomes List of study names to search for, obtained from \code{available_outcomes}
+#' @param user="mruser" User name
+#' @param password="TMG_F1WnTL" Password
+#' @param dbname="mrbase" Database
+#' @param host="epi-franklin.epi.bris.ac.uk" Host
+#' @export
+#' @return Dataframe of summary statistics for 
+extract_outcome_data <- function(exposure_dat, outcomes, user="mruser", password="TMG_F1WnTL", dbname="mrbase", host="epi-franklin.epi.bris.ac.uk")
+{
+	snps <- paste(exposure_dat$SNP, collapse="', '")
+	outcomes <- paste(outcomes, collapse="', '")
+
+	mydb <- dbConnect(MySQL(), user=user, password=password, dbname=dbname, host=host)
+
+	query <- paste(
+		"SELECT a.*, b.*, c.*",
+		"FROM assoc a, snps b, study c",
+		"WHERE a.snp=b.id AND a.study=c.id",
+		"AND b.name IN ('", snps, "')",
+		"AND c.filename IN ('", outcomes, "')",
+		"ORDER BY filename;")
+
+	out <- dbSendQuery(mydb, query)
+	d <- fetch(out, n=-1)
+
+	d <- subset(d, select=c(name, beta, se, n, p, freq, effect, other, filename,))
+	names(d) <- c("SNP", "beta.outcome", "se.outcome", "samplesize.outcome", "pval.outcome", "eaf.outcome", "effect_allele.outcome", "other_allele.outcome", "outcome")
+
+	index <- d$se.outcome==0
+	d$se[index] <- d$beta.outcome[index] / pt(d$pval.outcome[index] / 2, df = d$n[index], low=FALSE)
 	return(outcome_dat)
 }
+
