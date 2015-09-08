63c8725071138adb3a156c5b98407534c0832b26
Gibran Hemani <explodecomputer@gmail.com>
2015-09-08 21:32:27 +0100

    updated doc

--- tests/workflow.R
+++ tests/workflow.R
@@ -1,10 +1,10 @@
 # system.file("data/telomere_length.txt", package="meffil")
 
-exposure_dat <- read_exposure_data("inst/data/telomere_length.txt", "Telomere length")
+exposure_dat <- read_exposure_data("~/repo/mr_base/inst/data/telomere_length.txt", "Telomere length")
 
 outcome_dat <- rbind(
-	read_outcome_data("inst/data/cardiogram.txt", "Cardiogram"),
-	read_outcome_data("inst/data/bladdercancer.txt", "Bladder cancer")
+	read_outcome_data("~/repo/mr_base/inst/data/cardiogram.txt", "Cardiogram"),
+	read_outcome_data("~/repo/mr_base/inst/data/bladdercancer.txt", "Bladder cancer")
 )
 
 # exposure_dat <- ld_pruning(exposure_dat)
