b28bdd9cf438fe90b8b5c29dbb90dc37e130a5ec
Gibran Hemani <explodecomputer@gmail.com>
2015-09-09 17:58:36 +0100

    remote login of mysql via tunnel now possible

--- tests/mysql.R
+++ tests/mysql.R
@@ -1,6 +1,11 @@
 mysql -u epxjz -h epi-franklin.epi.bris.ac.uk -p
 wv-92n_YjB
 
+mysql -u mruser -h epi-franklin.epi.bris.ac.uk -p
+TMG_F1WnTL
+
+mysql -u gh13047 -h epi-franklin.epi.bris.ac.uk -p
+ri.K-2Gbvd
 
 mydb <- dbConnect(MySQL(), user='epxjz', password='wv-92n_YjB', dbname='mrbase', host='epi-franklin.epi.bris.ac.uk')
 dbListTables(mydb)
@@ -23,6 +28,12 @@ d <- fetch(rs, n=10)
 dim(d)
 
 
+ssh -L 3306:localhost:3306 gh13047@epi-franklin.epi.bris.ac.uk
+mysql -u gh13047 -h 127.0.0.1 -P 3306 -p
+ri.K-2Gbvd
+
+mysql -u mruser -h 127.0.0.1 -P 3306 -p
+TMG_F1WnTL
 
 use mrbase;
 
@@ -54,3 +65,7 @@ WHERE a.snp=b.id AND a.study=c.id
 AND b.name IN ('rs10900000', 'rs10000010', 'rs10000092')
 AND c.filename IN ('cardiogramplusc4d_180814_update_data.txt.uniform.af.txt', 'All_ancestries_SNP_gwas_mc_merge_nogc.tbl.uniq.gz.uniform.af.txt', 'MAGIC_INSULIN_SECRETION_DI_for_release_HMrel27.txt.uniform.af.txt')
 ORDER BY filename;
+
+
+
+
