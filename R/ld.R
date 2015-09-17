a0bd1f382123adadabbda1f454c19532e0544fc2
Gibran Hemani <explodecomputer@gmail.com>
2015-09-17 19:44:29 +0100

    fixed snp patching in ld functions

--- R/ld.R
+++ R/ld.R
@@ -7,7 +7,7 @@ plink_clump <- function(snps, pvals, refdat, clump_kb, clump_r2, clump_p1, clump
 	fun2 <- paste(plink_bin, " --bfile ", refdat, " --clump ", fn, " --clump-p1 ", clump_p1, " --clump-p2 ", clump_p2, " --clump-r2 ", clump_r2, " --clump-kb ", clump_kb, " --out ", fn, sep="")
 	system(fun2)
 	a <- read.table(paste(fn, ".clumped", sep=""), he=T)
-	# unlink(paste(fn, "*", sep=""))
+	unlink(paste(fn, "*", sep=""))
 	return(a)
 }
 
@@ -27,15 +27,19 @@ plink_clump <- function(snps, pvals, refdat, clump_kb, clump_r2, clump_p1, clump
 ld_pruning_all <- function(dat, refdat="data_maf0.01", clump_kb=10000, clump_r2=0.1, clump_p1=1, clump_p2=1, plink_bin="plink1.90", tempdir = ".")
 {
 	dat$p.exposure <- pnorm(abs(dat$beta.exposure / dat$se.exposure), lower.tail=FALSE)
+	snpcode <- paste("chr", dat$chr_name, ":", dat$chrom_start, sep="")
 	res <- plink_clump(
-		paste("chr", dat$chr_name, ":", dat$chrom_start, sep=""),
+		snpcode,
 		dat$p.exposure,
 		refdat,
 		clump_kb, clump_r2, clump_p1, clump_p2, plink_bin, tempdir)
+
+	index <- match(res$SNP, snpcode)
+	res$SNP <- dat$SNP[index]
 	y <- subset(dat, !SNP %in% res$SNP)
 	if(nrow(y) > 0)
 	{
-		message("Removing", y$SNP, "using clumping due to LD with other SNPs")
+		message("Removing the following SNPs due to LD with other SNPs:", paste(y$SNP, collapse="\n"), sep="\n")
 	}
 	return(subset(dat, SNP %in% res$SNP))
 }
