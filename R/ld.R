1b65e283e11d9e60a02091d65ea6891b9d29372b
Gibran Hemani <explodecomputer@gmail.com>
2015-09-17 18:58:16 +0100

    updated ld stuff

--- R/ld.R
+++ R/ld.R
@@ -4,33 +4,40 @@ plink_clump <- function(snps, pvals, refdat, clump_kb, clump_r2, clump_p1, clump
 	fn <- tempfile(tmpdir = tempdir)
 	write.table(data.frame(SNP=snps, P=pvals), file=fn, row=F, col=T, qu=F)
 
-	fun1 <- paste(plink_bin, " --bfile ", refdat, " --extract ", fn, " --make-bed --out ", fn, sep="")
-	system(fun1)
-
-	fun2 <- paste(plink_bin, " --bfile ", fn, " --clump ", fn, " --clump-p1 ", clump_p1, " --clump-p2 ", clump_p2, " --clump-r2 ", clump_r2, " --clump-kb ", clump_kb, " --out ", fn, sep="")
+	fun2 <- paste(plink_bin, " --bfile ", refdat, " --clump ", fn, " --clump-p1 ", clump_p1, " --clump-p2 ", clump_p2, " --clump-r2 ", clump_r2, " --clump-kb ", clump_kb, " --out ", fn, sep="")
 	system(fun2)
 	a <- read.table(paste(fn, ".clumped", sep=""), he=T)
-	unlink(paste(fn, "*", sep=""))
+	# unlink(paste(fn, "*", sep=""))
 	return(a)
 }
 
-# Use clumping in plink2
-ld_pruning <- function(dat, refdat=paste("/path/chr", 1:22, sep=""), clump_kb=10000, clump_r2=0.1, clump_p1=1, clump_p2=1, plink_bin, tempdir = ".")
+#' Perform clumping on the chosen SNPs
+#'
+#' @param dat Output from \code{read_exposure_data}
+#' @param refdat="data_maf0.01" Location of reference genotype data
+#' @param clump_kb=10000 Clumping window 
+#' @param clump_r2=0.1 Clumping r2 cutoff
+#' @param clump_p1=1 Clumping sig level for index SNPs
+#' @param clump_p2=1 Clumping sig level for secondary SNPs
+#' @param plink_bin="plink1.90" plink2 exe
+#' @param tempdir = "." Where to print results
+#'
+#' @export
+#' @return Data frame of only independent SNPs
+ld_pruning_all <- function(dat, refdat="data_maf0.01", clump_kb=10000, clump_r2=0.1, clump_p1=1, clump_p2=1, plink_bin="plink1.90", tempdir = ".")
 {
-
 	dat$p.exposure <- pnorm(abs(dat$beta.exposure / dat$se.exposure), lower.tail=FALSE)
-	ddply(dat, .(chr_name), function(x)
+	res <- plink_clump(
+		paste("chr", dat$chr_name, ":", dat$chrom_start, sep=""),
+		dat$p.exposure,
+		refdat,
+		clump_kb, clump_r2, clump_p1, clump_p2, plink_bin, tempdir)
+	y <- subset(dat, !SNP %in% res$SNP)
+	if(nrow(y) > 0)
 	{
-		res <- plink_clump(
-			paste(x$chr_name, ":", x$chrom_start, sep=""),
-			dat$p.exposure,
-			refdat[as.numeric(x$chr_name)],
-			clump_kb, clump_r2, clump_p1, clump_p2, plink_bin, tempdir)
-		y <- subset(x, SNP %in% res$SNP)
-		if(nrow(y) > 0)
-		{
-			message("Removing", y$SNP, "using clumping due to LD with other SNPs")
-		}
-		return(subset(x, SNP %in% res$SNP))
-	})
+		message("Removing", y$SNP, "using clumping due to LD with other SNPs")
+	}
+	return(subset(dat, SNP %in% res$SNP))
 }
+
+
