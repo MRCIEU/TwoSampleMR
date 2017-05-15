2b981b50299cf64391f0031b6b8ca0df2e7120c5
Gibran Hemani <explodecomputer@gmail.com>
2017-05-15 15:39:10 +0100

    updating mr function

--- R/mr.R
+++ R/mr.R
@@ -64,7 +64,7 @@ mr_method_list <- function()
 			name="Fixed effects meta analysis (simple SE)",
 			PubmedID="",
 			Description="",
-			use_by_default=TRUE,
+			use_by_default=FALSE,
 			heterogeneity_test=FALSE
 		),
 		list(
@@ -72,7 +72,7 @@ mr_method_list <- function()
 			name="Fixed effects meta analysis (delta method)",
 			PubmedID="",
 			Description="",
-			use_by_default=TRUE,
+			use_by_default=FALSE,
 			heterogeneity_test=TRUE
 		),
 		list(
@@ -80,7 +80,7 @@ mr_method_list <- function()
 			name="Random effects meta analysis (delta method)",
 			PubmedID="",
 			Description="",
-			use_by_default=TRUE,
+			use_by_default=FALSE,
 			heterogeneity_test=TRUE
 		),
 		list(
@@ -88,7 +88,7 @@ mr_method_list <- function()
 			name="Maximum likelihood",
 			PubmedID="",
 			Description="",
-			use_by_default=TRUE,
+			use_by_default=FALSE,
 			heterogeneity_test=TRUE
 		),
 		list(
@@ -138,6 +138,14 @@ mr_method_list <- function()
 			Description="",
 			use_by_default=TRUE,
 			heterogeneity_test=TRUE
+		),
+		list(
+			obj="mr_weighted_mode",
+			name="Weighted mode",
+			PubmedID="",
+			Description="",
+			use_by_default=TRUE,
+			heterogeneity_test=FALSE
 		)
 	)
 	a <- lapply(a, as.data.frame)
@@ -654,6 +662,131 @@ mr_penalised_weighted_median <- function(b_exp, b_out, se_exp, se_out, parameter
 }
 
 
+#' MR weighted mode estimators
+#'
+#' Weighted mode estimator
+#'
+#' @param dat Output from harmonise_data()
+#' @param parameters=default_parameters() <what param does>
+#'
+#' @export
+#' @return data frame
+mr_weighted_mode <- function(b_exp, b_out, se_exp, se_out, parameters=default_parameters()) 
+{
+	index <- !is.na(b_exp) & !is.na(b_out) & !is.na(se_exp) & !is.na(se_out)
+	if(sum(index) < 3)
+	return(list(b=NA, se=NA, pval=NA, nsnp=NA))
+
+	b_exp <- b_exp[index]
+	b_out <- b_out[index]
+	se_exp <- se_exp[index]
+	se_out <- se_out[index]
+
+	#--------------------------------------#
+	#Function to compute the point estimate#
+	#--------------------------------------#
+	#BetaIV.in: ratio estimates
+	#seBetaIV.in: standard errors of ratio estimates
+	beta <- function(BetaIV.in, seBetaIV.in, phi)
+	{
+		#Bandwidth rule - modified Silverman's rule proposed by Bickel (2002)
+		s <- 0.9*(min(sd(BetaIV.in), mad(BetaIV.in)))/length(BetaIV.in)^(1/5)
+
+		#Standardised weights
+		weights <- seBetaIV.in^-2/sum(seBetaIV.in^-2)
+
+		beta <- NULL
+
+		for(cur_phi in phi)
+		{
+			#Define the actual bandwidth
+			h <- s*cur_phi
+			#Compute the smoothed empirical density function
+			densityIV <- density(BetaIV.in, weights=weights, bw=h)
+			#Extract the point with the highest density as the point estimate 
+			beta[length(beta)+1] <- densityIV$x[densityIV$y==max(densityIV$y)]
+		}
+		return(beta)
+	}
+
+	#------------------------------------------#
+	#Function to estimate SEs through bootstrap#
+	#------------------------------------------#
+	#BetaIV.in: ratio estimates
+	#seBetaIV.in: standard errors of ratio estimates
+	#beta_Mode.in: point causal effect estimates
+	boot <- function(BetaIV.in, seBetaIV.in, beta_Mode.in, nboot)
+	{
+		#Set up a matrix to store the results from each bootstrap iteration
+		beta.boot <- matrix(nrow=nboot, ncol=length(beta_Mode.in))
+
+		for(i in 1:nboot) 
+		{
+			#Re-sample each ratio estimate using SEs derived not assuming NOME
+			BetaIV.boot      <- rnorm(length(BetaIV.in), mean=BetaIV.in, sd=seBetaIV.in[,1])
+			#Re-sample each ratio estimate using SEs derived under NOME
+			BetaIV.boot_NOME <- rnorm(length(BetaIV.in), mean=BetaIV.in, sd=seBetaIV.in[,2])
+
+			#Simple mode, not assuming NOME
+			beta.boot[i,1:length(phi)] <- beta(BetaIV.in=BetaIV.boot, seBetaIV.in=rep(1, length(BetaIV)), phi=phi)
+			#Weighted mode, not assuming NOME
+			beta.boot[i,(length(phi)+1):(2*length(phi))] <- beta(BetaIV.in=BetaIV.boot, seBetaIV.in=seBetaIV.in[,1], phi=phi)
+			#Simple mode, assuming NOME
+			beta.boot[i,(2*length(phi)+1):(3*length(phi))] <- beta(BetaIV.in=BetaIV.boot_NOME, seBetaIV.in=rep(1, length(BetaIV)), phi=phi)
+			#Weighted mode, assuming NOME
+			beta.boot[i,(3*length(phi)+1):(4*length(phi))] <- beta(BetaIV.in=BetaIV.boot_NOME, seBetaIV.in=seBetaIV.in[,2], phi=phi)
+		}
+		return(beta.boot)
+	}
+
+	# Parameters
+	phi <- parameters$phi
+	nboot <- parameters$nboot
+	alpha <- parameters$alpha
+
+	#Ratio estimates
+	BetaIV   <- b_out/b_exp
+
+	#SEs of ratio estimates
+	seBetaIV <- cbind(sqrt((se_out^2)/(b_exp^2) + ((b_out^2)*(se_exp^2))/(b_exp^4)), #SEs NOT assuming NOME
+	se_out/abs(b_exp)) #SEs ASSUMING NOME
+
+	#Point causal effect estimate using the simple mode
+	beta_SimpleMode <- beta(BetaIV.in=BetaIV, seBetaIV.in=rep(1, length(BetaIV)), phi=phi)
+
+	#Point causal effect estimate using the weighted mode (not asusming NOME)
+	beta_WeightedMode <- beta(BetaIV.in=BetaIV, seBetaIV.in=seBetaIV[,1], phi=phi)
+
+	#Point causal effect estimate using the weighted mode (asusming NOME)
+	beta_WeightedMode_NOME <- beta(BetaIV.in=BetaIV, seBetaIV.in=seBetaIV[,2], phi=phi)
+
+	#Combine all point effect estimates in a single vector
+	beta_Mode <- rep(c(beta_SimpleMode, beta_WeightedMode,
+	beta_SimpleMode, beta_WeightedMode_NOME))
+
+	#Compute SEs, confidence intervals and P-value
+	beta_Mode.boot <- boot(BetaIV.in=BetaIV, seBetaIV.in=seBetaIV, beta_Mode.in=beta_Mode, nboot=nboot)
+	se_Mode <- apply(beta_Mode.boot, 2, mad)
+
+	CIlow_Mode <- beta_Mode-qnorm(1-alpha/2)*se_Mode
+	CIupp_Mode <- beta_Mode+qnorm(1-alpha/2)*se_Mode
+
+	P_Mode <- pt(abs(beta_Mode/se_Mode), df=length(b_exp)-1, lower.tail=F)*2
+
+	#Vector to indicate the method referring to each row
+	Method <- rep(c('Simple mode', 'Weighted mode', 'Simple mode (NOME)', 'Weighted mode (NOME)'), each=length(phi))
+
+	#Return a data frame containing the results
+	Results <- data.frame(Method, length(b_exp), beta_Mode, se_Mode, CIlow_Mode, CIupp_Mode, P_Mode)
+	colnames(Results) <- c('Method', 'nsnp', 'Estimate', 'SE', 'CI_low', 'CI_upp', 'P')
+
+	i <- which(Method == "Weighted mode")
+
+	return(list(b = Results$Estimate[i], se = Results$SE[i], pval=Results$P[i], nsnp=length(b_exp)))
+}
+
+
+
 #' MR median estimators
 #'
 #' @param dat Output from harmonise_data()
@@ -1053,6 +1186,10 @@ mr_mode <- function(dat, parameters=default_parameters())
 
 	return(Results)
 }
+
+
+
+
 
 
 #' Perform Rucker, Median and Mode
