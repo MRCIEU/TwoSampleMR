afc54a44a2355c1009fb3ecf1554d3cdcab96516
remlapmot <remlapmot@hotmail.com>
2017-08-22 11:10:57 +0100

    corrected code wrt to dlply loop

--- R/plots.R
+++ R/plots.R
@@ -31,6 +31,7 @@ mr_scatter_plot <- function(mr_results, dat, ci=FALSE)
 			mrres$a[mrres$method == "MR Egger"] <- temp$b_i
 			if (ci==TRUE) {
 				eggerci <- as.data.frame(predict(temp$mod, interval="confidence", level=0.95))
+                                d <- cbind(d, eggerci)
 			} 
 		}
 
@@ -40,24 +41,23 @@ mr_scatter_plot <- function(mr_results, dat, ci=FALSE)
 			mrres$a[mrres$method == "MR Egger (bootstrap)"] <- temp$b_i
 		}
 
-		mr_scatter_plot <- 
-		ggplot2::ggplot(data=d, ggplot2::aes(x=beta.exposure, y=beta.outcome)) +
+		mr_scatter_plot <- ggplot2::ggplot(data=d, ggplot2::aes(x=beta.exposure, y=beta.outcome)) +
 			ggplot2::geom_errorbar(ggplot2::aes(ymin=beta.outcome-se.outcome, ymax=beta.outcome+se.outcome), colour="grey", width=0) +
 			ggplot2::geom_errorbarh(ggplot2::aes(xmin=beta.exposure-se.exposure, xmax=beta.exposure+se.exposure), colour="grey", height=0) +
 			ggplot2::geom_point() +
 			ggplot2::geom_abline(data=mrres, ggplot2::aes(intercept=a, slope=b, colour=method), show.legend=TRUE) +
 			ggplot2::scale_colour_manual(values=c("#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a", "#ffff99", "#b15928")) +
-			ggplot2::labs(colour="MR Test", x=paste("SNP effect on", d$exposure[1]), y=paste("SNP effect on", d$outcome[1])) +
+			ggplot2::labs(colour="MR Estimator", x=paste("SNP effect on", d$exposure[1]), y=paste("SNP effect on", d$outcome[1])) +
 			ggplot2::theme(legend.position="top", legend.direction="vertical") +
 			ggplot2::guides(colour=ggplot2::guide_legend(ncol=2))
-		if (ci==TRUE) {
-			mr_scatter_plot <- mr_scatter_plot + 
-				ggplot2::geom_line(data=eggerci, aes(y=lwr), linetype="dashed", colour=method) + 
-				ggplot2::geom_line(data=eggerci, aes(y=upr), linetype="dashed", colour=method)
-		}
-		mr_scatter_plot
+                if (ci == TRUE & "MR Egger" %in% mrres$method) {
+                        mr_scatter_plot <- mr_scatter_plot + 
+                                ggplot2::geom_line(ggplot2::aes(y=lwr), linetype="dashed", colour="#1f78b4") + 
+                                ggplot2::geom_line(ggplot2::aes(y=upr), linetype="dashed", colour="#1f78b4")
+                }
+                mr_scatter_plot
 	})
-	mrres
+        mrres
 }
 
 
