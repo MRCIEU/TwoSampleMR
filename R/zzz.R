e586ed605ca54e3aa4f1e64bb4a7b1334f4e8b90
Gibran Hemani <explodecomputer@gmail.com>
2017-10-25 16:38:27 +0100

    fixed problem with httr authentication

--- R/zzz.R
+++ R/zzz.R
@@ -26,7 +26,8 @@
 
 	# options(mrbaseapi="http://scmv-webapps.epi.bris.ac.uk:5000/")
 	options(mrbaseapi="http://api.mrbase.org/")
-	
+	options(googleAuthR.httr_oauth_cache = ".httr-oauth")
+
 	invisible()
 
 }
