bec1ea707a5c6ad9dfb81e7b90add7cf6d77fce4
Gibran Hemani <explodecomputer@gmail.com>
2018-08-04 08:47:16 +0100

    updated sign test

--- tests/test_mr_sign.R
+++ tests/test_mr_sign.R
@@ -21,12 +21,12 @@ library(dplyr)
 group_by(param, n) %>% summarise(minp = min(pval))
 
 # Looks like 6 is the smallest number of SNPs that can achieve p < 0.05
-
+library(TwoSampleMR)
 a <- extract_instruments(2)
 b <- extract_outcome_data(a$SNP,7)
 dat <- harmonise_data(a, b)
 mr(dat, method_list=c("mr_ivw", "mr_sign"))
-
+mr_scatter_plot(mr(dat, method_list=c("mr_ivw", "mr_sign")), dat)
 
 # Could consider doing a parametric bootstrap type analysis
 
