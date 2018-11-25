f33f6fea3ce2b01280765e30c7c030e290e5f7c3
Gibran Hemani <explodecomputer@gmail.com>
2018-11-25 14:40:16 +0000

    working on vcf harmonisation:

--- R/harmonise.R
+++ R/harmonise.R
@@ -24,6 +24,8 @@
 #' - other_allele.exposure
 #' - eaf.exposure
 #'
+#' The function tries to harmonise INDELs. If they are coded as sequence strings things work more smoothly. If they are coded as D/I in one dataset it will try to convert them to sequences if the other dataset has adequate information. If coded as D/I in one dataset and as a variant with equal length INDEL alleles in the other, the variant is dropped. If one or both the datasets only has one allele (i.e. the effect allele) then harmonisation is naturally going to be more ambiguous and more variants will be dropped.
+#'
 #' @param exposure_dat Output from \code{read_exposure_data}
 #' @param outcome_dat Output from \code{extract_outcome_data}
 #' @param action Level of strictness in dealing with SNPs. 1=Assume all reference alleles are on the positive strand, i.e. do nothing (warning - this is very risky and is not recommended); 2=Try to infer positive strand alleles, using allele frequencies for palindromes; 3=Correct strand for non-palindromic SNPs, and drop all palindromic SNPs from the analysis. If a single value is passed then this action is applied to all outcomes. But multiple values can be supplied as a vector, each element relating to a different outcome.
---
+++ vignettes/vcf/harmonise_ref.R
@@ -0,0 +1,187 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library(argparse))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(TwoSampleMR))
+suppressPackageStartupMessages(library(data.table))
+
+
+# create parser object
+parser <- ArgumentParser()
+
+parser$add_argument('--ref-file', required=TRUE)
+parser$add_argument('--ref-build', required=TRUE, default="b37")
+parser$add_argument('--gwas-file', required=TRUE)
+parser$add_argument('--gwas-header', required=TRUE, defailt=FALSE)
+parser$add_argument('--gwas-snp', type="integer", required=TRUE)
+parser$add_argument('--gwas-ref', type="integer", required=FALSE)
+parser$add_argument('--gwas-alt', type="integer", required=TRUE)
+parser$add_argument('--gwas-af', type="integer", required=FALSE)
+parser$add_argument('--gwas-beta', type="integer", required=FALSE)
+parser$add_argument('--gwas-se', type="integer", required=FALSE)
+parser$add_argument('--gwas-pval', type="integer", required=FALSE)
+parser$add_argument('--gwas-n0', type="integer", required=FALSE)
+parser$add_argument('--gwas-n1', type="integer", required=FALSE)
+parser$add_argument('--out', required=TRUE)
+
+args <- parser$parse_args()
+
+print(args)
+
+read_dat <- function(filename, type, header, snp, ref, alt, af, beta, se, pval, n0, n1)
+{
+	if(grepl("gz$", filename))
+	{
+		dat <- data.table::fread(paste0("gunzip -c ", filename), header=header)
+	} else {
+		dat <- data.table::fread(filename, header=header)
+	}
+	nc <- ncol(dat)
+	if(is.na(snp))
+	{
+		dat[[paste0("V", ncol(dat)+1)]] <- rep(NA, nrow(dat))
+		snp <- ncol(dat)
+	}
+	if(is.na(ref))
+	{
+		dat[[paste0("V", ncol(dat)+1)]] <- rep(NA, nrow(dat))
+		ref <- ncol(dat)
+	}
+	if(is.na(alt))
+	{
+		dat[[paste0("V", ncol(dat)+1)]] <- rep(NA, nrow(dat))
+		alt <- ncol(dat)
+	}
+	if(is.na(af))
+	{
+		dat[[paste0("V", ncol(dat)+1)]] <- rep(NA, nrow(dat))
+		af <- ncol(dat)
+	}
+	if(is.na(beta))
+	{
+		dat[[paste0("V", ncol(dat)+1)]] <- rep(NA, nrow(dat))
+		beta <- ncol(dat)
+	}
+	if(is.na(se))
+	{
+		dat[[paste0("V", ncol(dat)+1)]] <- rep(NA, nrow(dat))
+		se <- ncol(dat)
+	}
+	if(is.na(pval))
+	{
+		dat[[paste0("V", ncol(dat)+1)]] <- rep(NA, nrow(dat))
+		pval <- ncol(dat)
+	}
+	if(is.na(n0))
+	{
+		dat[[paste0("V", ncol(dat)+1)]] <- rep(NA, nrow(dat))
+		n0 <- ncol(dat)
+	}
+	if(is.na(n1))
+	{
+		dat[[paste0("V", ncol(dat)+1)]] <- rep(NA, nrow(dat))
+		n1 <- ncol(dat)
+	}
+
+	o <- format_data(
+		dat, 
+		type=type, 
+		phenotype_col=type,
+		snp_col=names(dat)[snp],
+		beta_col=names(dat)[beta],
+		se_col=names(dat)[se],
+		effect_allele_col=names(dat)[alt],
+		other_allele_col=names(dat)[ref],
+		eaf_col=names(dat)[af],
+		pval_col=names(dat)[pval],
+		ncase_col=names(dat)[n1],
+		ncontrol_col=names(dat)[n0]
+	)
+	return(o)
+}
+
+
+
+
+# Read in gwas data
+gwas <- read_dat(
+	args[["gwas-file"]],
+	type="outcome",
+	header=args[["gwas-header"]],
+	snp=args[["gwas-snp"]],
+	ref=args[["gwas-ref"]],
+	alt=args[["gwas-alt"]],
+	af=args[["gwas-af"]],
+	beta=args[["gwas-beta"]],
+	se=args[["gwas-se"]],
+	pval=args[["gwas-pval"]],
+	n0=args[["gwas-n0"]],
+	n1=args[["gwas-n1"]]
+)
+
+
+# Read in ref
+
+ref <- data.table::fread(paste0("gunzip -c ", args[["ref"]]))
+stopifnot(c("CHROM", "ID", "REF", "ALT", "AF", "POS") %in% names(ref))
+
+# For simplicity just keeping SNP Ids that are in common
+ref <- subset(ref, ID %in% gwas$snp_col)
+
+# Put in some dummy variables for the reference for harmonising
+ref$beta <- 1
+ref$se <- 0.1
+ref$pval <- 0.1
+a <- TwoSampleMR::format_data(
+        ref,
+        type="exposure",
+        snp_col="ID",
+        effect_allele_col="ALT",
+        other_allele_col="REF",
+        eaf_col="MAF"
+)
+
+# Check strand
+action <- TwoSampleMR::is_forward_strand(gwas$SNP, gwas$effect_allele.exposure, gwas$other_allele.exposure, ref$ID, ref$ALT, ref$REF, threshold=0.9)
+
+# Harmonise
+dat <- TwoSampleMR::harmonise_data(a, gwas, action)
+
+
+gwas_h <- ab %$%
+	dplyr::data_frame(
+		ID=SNP,
+		ALT=effect_allele.exposure,
+		REF=other_allele.exposure,
+		BETA=beta.outcome,
+		SE=se.outcome,
+		PVALUE=pval.outcome,
+		AF=eaf.outcome,
+		N=samplesize.outcome,
+		NCASE=ncase.outcome,
+		NCONTROL=ncontrol.outcome) %>%
+	dplyr::inner_join(subset(ref, select=c(ID,REF,ALT,CHROM,POS,AF)), by=c("ID", "REF", "ALT"))
+
+
+# Create vcf format
+vcf <- TwoSampleMR::make_vcf(
+                ID = gwas_h$ID,
+                ALT = gwas_h$ALT,
+                REF = gwas_h$REF,
+                B = gwas_h$BETA,
+                SE = gwas_h$SE,
+                PVAL = gwas_h$PVALUE,
+                N0 = gwas_h$NCONTROL,
+                N1 = gwas_h$NCASE,
+                CHROM = gwas_h$CHROM,
+                POS = gwas_h$POS,
+                AF = gwas_h$AF,
+                QUAL = rep(NA, nrow(gwas_h)),
+                FILTER = rep("PASS", nrow(gwas_h)),
+                build = args[["ref-build"]]
+        )
+
+# Write vcf
+TwoSampleMR::write_vcf(vcf, args[["out"]])
+
+
