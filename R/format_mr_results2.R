213dd8a1794eb5f82e1df941e79c7941cae540ad
Philip Haycock <philip.haycock@bristol.ac.uk>
2019-06-10 18:00:42 +0100

    corrected power.prune function. now allows continous or binary outcome traits for methods 1 and 2.

--- R/format_mr_results2.R
+++ R/format_mr_results2.R
@@ -212,101 +212,149 @@ combine_all_mrresults <- function(res,het,plt,sin,ao_slc=T,Exp=F,split.exposure=
 
 #' Power prune 
 #'
-#' When there are duplicate summary sets for a particular exposure-outcome combination, this function keeps the exposure-outcome summary set with the highest expected statistical power. This can be done by dropping the duplicate summary sets with the smaller outcome sample sizes. Alternatively, the pruning procedure can take into account both instrument strength and outcome sample size. The latter is useful, for example, when there is considerable variation in SNP coverage between duplicate outcome GWAS (e.g. because some studies have used targeted or fine mapping arrays). If there are a large number of SNPs available to instrument an exposure, the outcome GWAS with the better SNP coverage may provide better power than the outcome GWAS with the larger sample size. However, this latter procedure should only be implemented if the SNP-outcome effects are log odds ratios and the SNP-exposure effects are in standard deviation units. 
-
+#' When there are duplicate summary sets for a particular exposure-outcome combination, this function keeps the exposure-outcome summary set with the highest expected statistical power. This can be done by dropping the duplicate summary sets with the smaller sample sizes. Alternatively, the pruning procedure can take into account instrument strength and outcome sample size. The latter is useful, for example, when there is considerable variation in SNP coverage between duplicate summary sets (e.g. because some studies have used targeted or fine mapping arrays). If there are a large number of SNPs available to instrument an exposure, the outcome GWAS with the better SNP coverage may provide better power than the outcome GWAS with the larger sample size. 
 #'
 #' @param dat Results from harmonise_data() 
-#' @param method.size  Should the duplicate summary sets be pruned on the basis of sample size? Default set to TRUE. If available, the method uses the number of cases to represent sample size. If the number of cases is missing then the total sample size is used. If method.size is set to FALSE then duplicates are dropped on the basis of expected statistical power, taking into account instrument strength and sample size, and assumes that the SNP-outcome effects are log odds ratios and that the SNP-exposure effects are in standard deviation units. 
+#' @param method Should the duplicate summary sets be pruned on the basis of sample size alone (method = 1) or a combination of instrument strength and sample size (method = 2)? Default set to 1. When set to 1, the duplicate summary sets are first dropped on the basis of the outcome sample size (smaller duplicates dropped). If duplicates are still present, remaining duplicates are dropped on the basis of the exposure sample size (smaller duplicates dropped). When method set to 2, duplicates are dropped on the basis of instrument strength (amount of variation explained in the exposure by the instrumental SNPs) and sample size, and assumes that the SNP-exposure effects correspond to a continuous trait with a normal distribution (i.e. exposure cannot be binary). The SNP-outcome effects can correspond to either a binary or continuous trait. If the exposure is binary then method 1 should be used. 
+
+#' @param dist.outcome The distribution of the outcome. Can either be binary or continuous. Default set to binary. 
 #' 
 #' @export
-#' @return data frame
+#' @return data.frame with duplicate summary sets removed
 
-# library(TwoSampleMR)
-# library(MRInstruments)
-
-# exp_dat <- extract_instruments(outcomes=c(2,300))
-
-# chd_out_dat <- extract_outcome_data(
-#     snps = exp_dat$SNP,
-#     outcomes = c(6,7,8,9)
-# )
-
-# dat <- harmonise_data(
-#     exposure_dat = exp_dat, 
-#     outcome_dat = chd_out_dat
-# )
-
-# dat2<-power.prune(dat,drop.duplicates=T)
-
-power.prune <- function(dat,method.size=T)
+power.prune <- function(dat,method=1,dist.outcome="binary")
 {
 
 	# dat[,c("eaf.exposure","beta.exposure","se.exposure","samplesize.outcome","ncase.outcome","ncontrol.outcome")]
-	drop.duplicates=T
-
-	if(method.size==T){
-		dat$ncase.outcome[is.na(dat$ncase.outcome)]<-dat$samplesize.outcome[is.na(dat$ncase.outcome)]
-		dat<-dat[order(dat$ncase.outcome,decreasing=T),]
-		id.expout<-paste(dat$exposure,dat$outcome)
-		id.keep<-id.expout[!duplicated(paste(dat$exposure,dat$originalname.outcome))]
-		# dat$power<-as.numeric(as.factor(-dat$ncase.outcome))
-		if(drop.duplicates==T){
-			dat<-dat[id.expout %in% id.keep,]
+	if(method==1){
+		L<-NULL 
+		id.sets<-paste(split_exposure(dat)$exposure,split_outcome(dat)$outcome)
+		id.set.unique<-unique(id.sets)
+		dat$id.set<-as.numeric(factor(id.sets))
+		for(i in 1:length(id.set.unique)){
+			# print(i)
+			print(paste("finding summary set for --", id.set.unique[i],"-- with largest sample size", sep=""))
+			dat1<-dat[id.sets == id.set.unique[i],]
+			id.subset<-paste(dat1$exposure,dat1$outcome)
+			id.subset.unique<-unique(id.subset)
+			dat1$id.subset<-as.numeric(factor(id.subset))
+			ncase<-dat1$ncase.outcome
+			if(any(is.na(ncase))){
+				ncase<-dat1$samplesize.outcome
+				if(dist.outcome=="binary") warning(paste("dist.outcome set to binary but case sample size is missing. Will use total sample size instead but power pruning may be less accurate"))
+			}
+			if(any(is.na(ncase))) stop("sample size missing for at least 1 summary set")
+			dat1<-dat1[order(ncase,decreasing=T),]
+			# id.expout<-paste(split_exposure(dat)$exposure,split_outcome(dat)$outcome)
+			ncase<-ncase[order(ncase,decreasing=T)]
+			# dat1$power.prune.ncase<-"drop"
+			# dat1$power.prune.ncase[ncase==ncase[1]]<-"keep"
+			dat1<-dat1[ncase==ncase[1],]
+			nexp<-dat1$samplesize.exposure
+			dat1<-dat1[order(nexp,decreasing=T),]
+			nexp<-nexp[order(nexp,decreasing=T)]
+			# dat1$power.prune.nexp<-"drop"
+			# dat1$power.prune.nexp[nexp==nexp[1]]<-"keep"
+			# dat1$power.prune<-"drop"
+			# dat1$power.prune[dat1$power.prune.ncase=="keep" & dat1$power.prune.nexp == "keep"]<-"keep"
+			# dat1<-dat1[,!names(dat1) %in% c("power.prune.ncase","power.prune.nexp")]
+			# dat1[,c("samplesize.exposure","ncase.outcome","exposure","outcome")]
+			dat1<-dat1[nexp==nexp[1],]
+			L[[i]]<-dat1		
 		}
-	}
-
-	if(method.size==F){
-		p<-dat$eaf.exposure #effect allele frequency
-		b<-abs(dat$beta.exposure) # effect of SNP on risk factor
-		se<-dat$se.exposure
-		# n<-dat$samplesize.exposure
-		n.cas<-dat$ncase.outcome
-		n.con<-dat$ncontrol.outcome
-		b1=log(1.05) # assumed log odds ratio
-		sig<-0.05 #alpha
-
-		var<-1 # variance of risk factor assumed to be 1 
-		# if(!vareq1){ #if variance not 1 then estimate standardized beta using this syntax
-		# 	z = b/se
-		# 	b = z/sqrt(2*p*(1-p)*(n+z^2))
-			
+		dat<-do.call(rbind,L)
+		dat<-dat[,!names(dat1) %in% c("id.set","id.subset")]
+		# if(drop.duplicates == T) {
+		# 	dat<-dat[dat$power.prune=="keep",]
 		# }
-
-		r2<-2*b^2*p*(1-p)/var
-
-		#Once the r2 is calculated you then sum the r2 statistics across all the SNPs in the instrument. 
-		#This assumes each SNP is independent. 
-		#calculate the F statistic for the instrument:
-		id<-paste(dat$exposure,dat$outcome)
-		power<-NULL
-		iv.se<-NULL
-		ID<-NULL
-		for(i in 1:length(unique(id))){
-			print(unique(id)[i])
-			pos<-which(id==unique(id)[i])
-			# print(pos)
-			k<-length(p[pos]) #number of SNPs in the instrument / associated with the risk factor
-			# n<-min(n) #sample size of the exposure/risk factor GWAS
-			r2sum<-sum(r2[pos]) # sum of the r-squares for each SNP in the instrument
-			# F<-r2sum*(n-1-k)/((1-r2sum*k )
-			n<-unique(n.con[pos]+n.cas[pos])
-			ratio<-unique(n.cas[pos]/n.con[pos])
-			iv.se[[i]]<-1/sqrt(unique(n.cas[pos])*unique(n.con[pos])*r2sum) #standard error of the IV should be proportional to this
-			power[[i]]<-pnorm(sqrt(n*r2sum*(ratio/(1+ratio))*(1/(1+ratio)))*b1-qnorm(1-sig/2))
-			ID[[i]]<-unique(id)[i]
-		}
-		power.tab<-data.frame(as.matrix(cbind(ID,power)),stringsAsFactors=F)
-		dat$id.expout<-paste(dat$exposure,dat$outcome)
-		dat<-merge(dat,power.tab,by.x="id.expout",by.y="ID")
-		dat<-dat[order(dat$power,decreasing=T),]
-		# unique(dat[,c("originalname.outcome","Power")])
-		if(drop.duplicates==T){
-			id.keep<-dat$id.expout[!duplicated(paste(dat$exposure,dat$originalname.outcome))]
-			dat<-dat[dat$id.expout %in% id.keep,]
-
-		}
+		return(dat)
 	}
-	return(dat)
+
+	if(method==2){
+		L<-NULL 
+		id.sets<-paste(split_exposure(dat)$exposure,split_outcome(dat)$outcome)
+		id.set.unique<-unique(id.sets)
+		dat$id.set<-as.numeric(factor(id.sets))
+		for(i in 1:length(id.set.unique)){
+			print(i)
+			print(id.set.unique[i])
+			dat1<-dat[id.sets == id.set.unique[i],]
+			# unique(dat1[,c("exposure","outcome")])
+			id.subset<-paste(dat1$exposure,dat1$outcome)
+			id.subset.unique<-unique(id.subset)
+			dat1$id.subset<-as.numeric(factor(id.subset))
+			L1<-NULL
+			for(j in 1:length(id.subset.unique)){
+				# print(j)
+				print(paste("estimating power for summary set: ",id.subset.unique[j],sep=""))
+				dat2<-dat1[id.subset ==id.subset.unique[j], ]
+				p<-dat2$eaf.exposure #effect allele frequency
+				# b<-abs(dat2$beta.exposure) # effect of SNP on risk factor
+				se<-dat2$se.exposure
+				z<-dat2$beta.exposure/dat2$se.exposure
+				n<-dat2$samplesize.exposure
+				b<-z/sqrt(2*p*(1-p)*(n+z^2))										
+				if(any(is.na(dat2$ncase.outcome))) stop(paste("number of cases missing for summary set: ",id.subset.unique[j],sep=""))
+				n.cas<-dat2$ncase.outcome
+				n.con<-dat2$ncontrol.outcome
+				var<-1 # variance of risk factor assumed to be 1 
+				r2<-2*b^2*p*(1-p)/var
+				if(any(is.na(r2))) warning("beta or allele frequency missing for some SNPs, which could affect accuracy of power pruning")
+				r2<-r2[!is.na(r2)]
+				# k<-length(p[!is.na(p)]) #number of SNPs in the instrument / associated with the risk factor
+				# n<-min(n) #sample size of the exposure/risk factor GWAS
+				r2sum<-sum(r2) # sum of the r-squares for each SNP in the instrument
+				# F<-r2sum*(n-1-k)/((1-r2sum*k )
+				if(dist.outcome == "continuous"){
+					iv.se<- 1/sqrt(unique(dat2$samplesize.outcome)*r2sum) #standard error of the IV should be proportional to this
+				}
+				if(dist.outcome == "binary"){
+					iv.se<-1/sqrt(unique(n.cas)*unique(n.con)*r2sum) #standard error of the IV should be proportional to this
+					if(any(is.na(n.cas)) | any(is.na(n.con))) {
+						warning("dist.outcome set to binary but number of cases or controls is missing. Will try using total sample size instead but power pruning will be less accurate")
+						iv.se<- 1/sqrt(unique(dat2$samplesize.outcome)*r2sum) 
+					}
+				}
+				# Power calculations to implement at some point
+				# iv.se<-1/sqrt(unique(n.cas)*unique(n.con)*r2sum) #standard error of the IV should be proportional to this
+				# n.outcome<-unique(n.con+n.cas)
+				# ratio<-unique(n.cas/n.con)
+				# sig<-alpha #alpha
+				# b1=log(or) # assumed log odds ratio
+				# power<-pnorm(sqrt(n.outcome*r2sum*(ratio/(1+ratio))*(1/(1+ratio)))*b1-qnorm(1-sig/2))				
+				dat2$iv.se<-iv.se
+				# dat2$power<-power
+				L1[[j]]<-dat2
+			}
+			L[[i]]<-do.call(rbind,L1)	
+		}
+		dat2<-do.call(rbind,L)
+		dat2<-dat2[order(dat2$id.set,dat2$iv.se),]
+		id.sets<-unique(dat2$id.set)
+		id.keep<-NULL
+		for(i in 1:length(id.sets)){
+			# print(i)
+			# print(id.sets[i])
+			id.temp<-unique(dat2[dat2$id.set==id.sets[i],c("id.set","id.subset")])
+			id.keep[[i]]<-paste(id.temp$id.set,id.temp$id.subset)[1]
+		}
+		
+		dat2$power.prune<-"drop"
+		dat2$power.prune[paste(dat2$id.set,dat2$id.subset) %in% id.keep]<-"keep"
+		# if(drop.duplicates == T) {
+			dat2<-dat2[dat2$power.prune=="keep",]
+		# }
+		dat2<-dat2[,!names(dat2) %in% c("iv.se","power.prune","id.set","id.subset")]
+		dat<-dat2
+		# dat2[,c("exposure","outcome","iv.se","power","id.set","id.subset","power.prune")]
+
+		# unique(dat2[order(dat2$id.set,dat2$id.subset),c("samplesize.exposure","ncase.outcome","exposure","outcome","iv.se","power","id.set","id.subset")])
+
+		# dat2[dat2$id.set==1,c("iv.se","id.set","id.subset")]
+
+		return(dat)
+		
+	}
 }
 
 #' Size prune 
