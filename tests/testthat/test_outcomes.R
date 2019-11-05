2382513a010506ac51669c35bdc45936a6209f2a
Gibran Hemani <explodecomputer@gmail.com>
2019-11-05 11:32:45 +0000

    cleaning up

--- tests/testthat/test_outcomes.R
+++ tests/testthat/test_outcomes.R
@@ -3,23 +3,23 @@ library(TwoSampleMR)
 
 test_that("outcomes", {
 	
-	a <- extract_instruments(7)
-	b <- extract_outcome_data(a$SNP, 2, proxies=FALSE)
+	expect_warning(a <- extract_instruments(7))
+	expect_warning(b <- extract_outcome_data(a$SNP, 2, proxies=FALSE))
 	expect_true(nrow(b) < 30 & nrow(b) > 15)
 
-	b <- extract_outcome_data(a$SNP, 2, proxies=TRUE)
+	expect_warning(b <- extract_outcome_data(a$SNP, 2, proxies=TRUE))
 	expect_true(nrow(b) > 30 & nrow(b) < nrow(a))
 
-	b <- extract_outcome_data(a$SNP, c(2, "a"), proxies=FALSE)
+	expect_warning(b <- extract_outcome_data(a$SNP, c(2, "a"), proxies=FALSE))
 	expect_true(nrow(b) < 30 & nrow(b) > 15)
 
-	b <- extract_outcome_data(a$SNP, c(2, "a"), proxies=TRUE)
+	expect_warning(b <- extract_outcome_data(a$SNP, c(2, "a"), proxies=TRUE))
 	expect_true(nrow(b) > 30 & nrow(b) < nrow(a))
 
-	b <- extract_outcome_data(a$SNP, c(2, 7), proxies=FALSE)
+	expect_warning(b <- extract_outcome_data(a$SNP, c(2, 7), proxies=FALSE))
 	expect_true(nrow(b) > 60)
 
-	b <- extract_outcome_data(a$SNP, c(2, 7), proxies=TRUE)
+	expect_warning(b <- extract_outcome_data(a$SNP, c(2, 7), proxies=TRUE))
 	expect_true(nrow(b) > 70)
 
 })
