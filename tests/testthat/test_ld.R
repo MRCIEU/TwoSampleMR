0111ab3c51dff66e6e36fefe619b2176bcce7993
Gibran Hemani <explodecomputer@gmail.com>
2020-01-16 14:21:42 +0000

    added test for multiple ids in clumping

--- tests/testthat/test_ld.R
+++ tests/testthat/test_ld.R
@@ -12,13 +12,22 @@ test_that("clump", {
 	expect_true(nrow(out) > 0)
 })
 
-
-
 b <- ld_matrix(out$SNP)
 
 test_that("matrix", {
 	expect_equal(nrow(b), nrow(out))
 	expect_equal(ncol(b), nrow(out))
 })
+
+
+
+a <- extract_instruments(c("ieu-a-2", "ieu-a-1001"), clump=FALSE)
+out <- clump_data(a)
+
+test_that("clump multiple", {
+	expect_equal(length(unique(a$id.exposure)), length(unique(out$id.exposure)))
+})
+
+
 
 
