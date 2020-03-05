91436876c63742f2856091e284b1eb927b709d49
Tom Palmer <remlapmot@hotmail.com>
2020-03-05 17:22:00 +0000

    importFrom cooks.distance

--- R/rucker.R
+++ R/rucker.R
@@ -91,7 +91,7 @@ mr_rucker <- function(dat, parameters=default_parameters())
 	return(res)
 }
 
-#' @importFrom stats coefficients
+#' @importFrom stats coefficients cooks.distance
 mr_rucker_internal <- function(dat, parameters=default_parameters())
 {
 	if("mr_keep" %in% names(dat)) dat <- subset(dat, mr_keep)
