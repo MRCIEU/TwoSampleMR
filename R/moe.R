1d9398e4fd7b9a739d2c1ff152877b6100f970f6
Tom Palmer <remlapmot@hotmail.com>
2020-03-05 17:47:38 +0000

    importFrom median

--- R/moe.R
+++ R/moe.R
@@ -17,7 +17,7 @@ Isq <- function(y,s)
 	return(Isq)
 }
 
-#' @importFrom stats influence.measures ks.test
+#' @importFrom stats influence.measures ks.test median
 system_metrics <- function(dat)
 {
 	# Number of SNPs
