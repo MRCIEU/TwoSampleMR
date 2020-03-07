e5d2b68c9c35d765580e4ca8dd6f0720af979ba6
Tom Palmer <remlapmot@hotmail.com>
2020-03-07 14:58:16 +0000

    add parameters option description

--- R/mr.R
+++ R/mr.R
@@ -862,6 +862,7 @@ mr_ivw <- function(b_exp, b_out, se_exp, se_out, parameters=default_parameters()
 #' @param b_out Vector of genetic effects on outcome
 #' @param se_exp Standard errors of genetic effects on exposure
 #' @param se_out Standard errors of genetic effects on outcome
+#' @param parameters List of parameters. The default is `default_parameters()`.
 #'
 #' @export
 #' @return List with the following elements:
