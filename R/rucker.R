e46861f9ef20f2c62d87f23ad2250f8d3b2b4351
Tom Palmer <remlapmot@hotmail.com>
2020-03-07 15:11:07 +0000

    helpfile formatting

--- R/rucker.R
+++ R/rucker.R
@@ -357,7 +357,7 @@ mr_rucker_bootstrap <- function(dat, parameters=default_parameters())
 #'
 #' @md
 #' @param dat Output from harmonise_data.
-#' @param parameters The default is `default_parameters()`.
+#' @param parameters List of parameters. The default is `default_parameters()`.
 #'
 #' @export
 #' @return List
