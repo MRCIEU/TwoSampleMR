699a5b1c7ad4a9b5bdb0b4a56a9160ac7510a48e
Tom Palmer <remlapmot@hotmail.com>
2020-03-08 11:33:35 +0000

    helpfile parameters option

--- R/mr.R
+++ R/mr.R
@@ -313,10 +313,12 @@ mr_meta_fixed_simple <- function(b_exp, b_out, se_exp, se_out, parameters)
 
 #' Perform 2 sample IV using fixed effects meta analysis and delta method for standard errors
 #'
-#' @param b_exp Vector of genetic effects on exposure
-#' @param b_out Vector of genetic effects on outcome
-#' @param se_exp Standard errors of genetic effects on exposure
-#' @param se_out Standard errors of genetic effects on outcome
+#' @md
+#' @param b_exp Vector of genetic effects on exposure.
+#' @param b_out Vector of genetic effects on outcome.
+#' @param se_exp Standard errors of genetic effects on exposure.
+#' @param se_out Standard errors of genetic effects on outcome.
+#' @param parameters List of parameters.
 #'
 #' @export
 #' @return List with the following elements:
