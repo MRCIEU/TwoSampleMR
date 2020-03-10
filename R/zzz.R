4184a7db45eefca9f966362329380394da4a2b91
Tom Palmer <remlapmot@hotmail.com>
2020-03-10 10:19:44 +0000

    specify utils::packageVersion

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname) {
 
         packageStartupMessage(
-                paste("TwoSampleMR version", packageVersion("TwoSampleMR"), "\n"),
+                paste("TwoSampleMR version", utils::packageVersion("TwoSampleMR"), "\n"),
                 "[>] IMPORTANT: Some datasets have been updated, and some are disabled while we update them.\n",
                 "    Apologies for this inconvenience, they will be back up as soon as possible.\n",
                 "    See news(package='TwoSampleMR') and https://gwas.mrcieu.ac.uk for more information\n",
