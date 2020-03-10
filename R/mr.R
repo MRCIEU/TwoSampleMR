4218eade35667b38aa9f13ae6ce682012ae66f9a
Tom Palmer <remlapmot@hotmail.com>
2020-03-10 14:08:13 +0000

    helpfile formatting

--- R/mr.R
+++ R/mr.R
@@ -232,6 +232,8 @@ mr_method_list <- function()
 
 
 #' List of parameters for use with MR functions
+#' 
+#' The default is `list(test_dist = "z", nboot = 1000, Cov = 0, penk = 20, phi = 1, alpha = 0.05, Qthresh = 0.05, over.dispersion = TRUE, loss.function = "huber")`.
 #'
 #' @export
 default_parameters <- function()
