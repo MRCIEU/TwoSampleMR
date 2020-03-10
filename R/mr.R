3bfb0ac876cec08fadf0f641aad8b5810f26a95b
Tom Palmer <remlapmot@hotmail.com>
2020-03-10 14:49:21 +0000

    helpfile formatting

--- R/mr.R
+++ R/mr.R
@@ -2,7 +2,7 @@
 #' Perform all Mendelian randomization tests
 #'
 #' @param dat Harmonised exposure and outcome data. Output from \code{\link{harmonise_data}}.
-#' @param parameters Parameters to be used for various MR methods. Default is output from \code{default_parameters}.
+#' @param parameters Parameters to be used for various MR methods. Default is output from [`default_parameters`].
 #' @param method_list List of methods to use in analysis. See \code{\link{mr_method_list}} for details.
 #'
 #' @export
