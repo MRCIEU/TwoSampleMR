ee7ac8e0de422b94148e0e576a067d719f1c3d2f
Gibran Hemani <explodecomputer@gmail.com>
2021-05-07 21:00:03 +0100

    updated ld docs

--- R/ld.R
+++ R/ld.R
@@ -1,6 +1,13 @@
 #' Perform LD clumping on SNP data
 #'
 #' Uses PLINK clumping method, where SNPs in LD within a particular window will be pruned. The SNP with the lowest p-value is retained.
+#' 
+#' @details
+#' This function interacts with the OpenGWAS API, which houses LD reference panels for the 5 super-populations in the 1000 genomes reference panel. It includes only bi-allelic SNPs with MAF > 0.01, so it's quite possible that a variant you want to include in the clumping process will be absent. If it is absent, it will be automatically excluded from the results.
+#' 
+#' You can check if your variants are present in the LD reference panel using ieugwasr::ld_reflookup()
+#'
+#' This function does put load on the OpenGWAS servers, which makes life more difficult for other users. We have implemented a method and made available the LD reference panels to perform clumping locally, see ieugwasr::ld_clump() and related vignettes for details.
 #'
 #' @md
 #' @param dat Output from [`format_data`]. Must have a SNP name column (SNP), SNP chromosome column (chr_name), SNP position column (chrom_start). If id.exposure or pval.exposure not present they will be generated.
@@ -52,9 +59,16 @@ clump_data <- function(dat, clump_kb=10000, clump_r2=0.001, clump_p1=1, clump_p2
 
 #' Get LD matrix for list of SNPs
 #'
-#' This function takes a list of SNPs and searches for them in 502 European samples from 1000 Genomes phase 3 data.
+#' This function takes a list of SNPs and searches for them in a specified super-population in the 1000 Genomes phase 3 reference panel.
 #' It then creates an LD matrix of r values (signed, and not squared).
 #' All LD values are with respect to the major alleles in the 1000G dataset. You can specify whether the allele names are displayed.
+#'
+#' @details
+#' The data used for generating the LD matrix includes only bi-allelic SNPs with MAF > 0.01, so it's quite possible that a variant you want to include will be absent. If it is absent, it will be automatically excluded from the results.
+#' 
+#' You can check if your variants are present in the LD reference panel using ieugwasr::ld_reflookup()
+#'
+#' This function does put load on the OpenGWAS servers, which makes life more difficult for other users, and has been limited to analyse only up to 500 variants at a time. We have implemented a method and made available the LD reference panels to perform the operation locally, see ieugwasr::ld_matrix() and related vignettes for details.
 #'
 #' @param snps List of SNPs.
 #' @param with_alleles Whether to append the allele names to the SNP names. The default is `TRUE`.
