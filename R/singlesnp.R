040796424d9472159bfae559a9879fe393b7b9a3
Tom Palmer <remlapmot@hotmail.com>
2023-05-03 06:06:40 +0100

    Formatting

--- R/singlesnp.R
+++ R/singlesnp.R
@@ -1,6 +1,6 @@
 #' Perform 2 sample MR on each SNP individually
 #'
-#' @param dat Output from [`harmonise_data`].
+#' @param dat Output from [harmonise_data()].
 #' @param parameters List of parameters. The default is `default_parameters()`.
 #' @param single_method Function to use for MR analysis. The default is `"mr_wald_ratio"`.
 #' @param all_method Functions to use for MR analysis. The default is `c("mr_ivw", "mr_egger_regression")`.
