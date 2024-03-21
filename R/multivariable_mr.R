5d75a1cd96fa8353b3313ffc5f40881887b8fbf1
Tom Palmer <remlapmot@hotmail.com>
2024-03-21 09:41:37 +0000

    Improve formatting

--- R/multivariable_mr.R
+++ R/multivariable_mr.R
@@ -252,7 +252,7 @@ mv_extract_exposures_local <- function(
 #' \item{exposure_se}{is the same as `exposure_beta`, but for standard errors.}
 #' \item{exposure_pval}{the same as `exposure_beta`, but for p-values.}
 #' \item{expname}{A data frame with two variables, `id.exposure` and `exposure` which are character strings.}
-#' \item{outcome_beta}{an array of effects for the outcome, corresponding to the SNPs in exposure_beta.}
+#' \item{outcome_beta}{an array of effects for the outcome, corresponding to the SNPs in `exposure_beta`.}
 #' \item{outcome_se}{an array of standard errors for the outcome.}
 #' \item{outcome_pval}{an array of p-values for the outcome.}
 #' \item{outname}{A data frame with two variables, `id.outcome` and `outcome` which are character strings.}
