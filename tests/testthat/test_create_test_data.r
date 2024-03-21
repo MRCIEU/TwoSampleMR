ae755137e017a8266ea720faebd506dc24d94a52
Tom Palmer <remlapmot@hotmail.com>
2024-03-21 11:12:19 +0000

    Insert data creation within a skipped test

--- tests/testthat/test_create_test_data.r
+++ tests/testthat/test_create_test_data.r
@@ -1,4 +1,5 @@
-skip()
+test_that("Create local data", {
+skip("Skip local data creation unless you have good access to the API.")
 
 exp_dat <- extract_instruments("ieu-a-2")
 out_dat <- extract_outcome_data(exp_dat$SNP, "ieu-a-7")
@@ -6,3 +7,4 @@ dat <- make_dat("ieu-a-2", "ieu-a-7") %>% add_metadata()
 dat2 <- make_dat()
 
 save(exp_dat, out_dat, dat, dat2, file=file.path("inst", "extdata", "test_commondata.RData"), compress = "xz")
+})
