bda26e03074fa73134ab2b85d817d9b9a417d282
Tom Palmer <remlapmot@hotmail.com>
2024-06-29 11:12:05 +0100

    Re-enable tests requiring API

--- tests/testthat/test_outcomes.R
+++ tests/testthat/test_outcomes.R
@@ -2,27 +2,32 @@ context("outcome")
 
 test_that("outcomes", {
 
-  skip("Skip unless you have good access to the API.")
-  skip_on_ci()
   skip_on_cran()
 
 	a <- extract_instruments("ieu-a-7")
+	if(inherits(a, "response")) skip("Server issues")
 	b <- extract_outcome_data(a$SNP, "ieu-a-2", proxies=FALSE)
+	if(inherits(b, "response")) skip("Server issues")
 	expect_true(nrow(b) < 30 & nrow(b) > 15)
 
 	b <- extract_outcome_data(a$SNP, "ieu-a-2", proxies=TRUE)
+	if(inherits(b, "response")) skip("Server issues")
 	expect_true(nrow(b) > 30 & nrow(b) < nrow(a))
 
 	b <- extract_outcome_data(a$SNP, c("ieu-a-2", "a"), proxies=FALSE)
+	if(inherits(b, "response")) skip("Server issues")
 	expect_true(nrow(b) < 30 & nrow(b) > 15)
 
 	b <- extract_outcome_data(a$SNP, c("ieu-a-2", "a"), proxies=TRUE)
+	if(inherits(b, "response")) skip("Server issues")
 	expect_true(nrow(b) > 30 & nrow(b) < nrow(a))
 
 	b <- extract_outcome_data(a$SNP, c("ieu-a-2", "ieu-a-7"), proxies=FALSE)
+	if(inherits(b, "response")) skip("Server issues")
 	expect_true(nrow(b) > 60)
 
 	b <- extract_outcome_data(a$SNP, c("ieu-a-2", "ieu-a-7"), proxies=TRUE)
+	if(inherits(b, "response")) skip("Server issues")
 	expect_true(nrow(b) > 70)
 
 })
