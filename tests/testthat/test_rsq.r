c41c745b3a17424ece90df06564825c36bb4518d
Tom Palmer <remlapmot@hotmail.com>
2024-06-29 11:09:36 +0100

    Re-enable tests requiring API

--- tests/testthat/test_rsq.r
+++ tests/testthat/test_rsq.r
@@ -10,8 +10,9 @@ test_that("exposure data 1", {
 })
 
 test_that("exposure data 2", {
-  skip("Skip unless you have good access to the API.")
-  d <- extract_instruments(c('ieu-a-2', 'ieu-a-7')) %>% add_rsq()
+  d <- extract_instruments(c('ieu-a-2', 'ieu-a-7'))
+  if(inherits(d, "response")) skip("Server issues")
+  d <- d %>% add_rsq()
 	expect_true("rsq.exposure" %in% names(d))
 	expect_true("effective_n.exposure" %in% names(d))
 })
@@ -19,31 +20,34 @@ test_that("exposure data 2", {
 exposure <- exp_dat[1:5,]
 
 test_that("outcome data 1", {
-  skip("Skip unless you have good access to the API.")
-  d <- extract_outcome_data(exposure$SNP, 'ieu-a-2') %>% add_rsq()
+  d <- extract_outcome_data(exposure$SNP, 'ieu-a-2')
+  if(inherits(d, "response")) skip("Server issues")
+  d <- d %>% add_rsq()
 	expect_true("rsq.outcome" %in% names(d))
 	expect_true("effective_n.outcome" %in% names(d))
 })
 
 test_that("outcome data 2", {
-  skip("Skip unless you have good access to the API")
-  d <- extract_outcome_data(exposure$SNP, c('ieu-a-2', 'ieu-a-7')) %>% add_rsq()
+  d <- extract_outcome_data(exposure$SNP, c('ieu-a-2', 'ieu-a-7'))
+  if(inherits(d, "response")) skip("Server issues")
+  d <- d %>% add_rsq()
 	expect_true("rsq.outcome" %in% names(d))
 	expect_true("effective_n.outcome" %in% names(d))
 })
 
 test_that("dat 2", {
-  skip("Skip unless you have good access to the API.")
-  d <- make_dat(proxies=FALSE) %>% add_rsq()
+  d <- make_dat(proxies=FALSE)
+  if(inherits(d, "response")) skip("Server issues")
+  d <- d %>% add_rsq()
 	expect_true("rsq.outcome" %in% names(d) & "rsq.exposure" %in% names(d))
 	expect_true("effective_n.outcome" %in% names(d) & "effective_n.exposure" %in% names(d))
 })
 
 test_that("dat ukb-d", {
-  skip("Skip unless you have good access to the API.")
-  skip_on_ci()
   skip_on_cran()
-  d <- make_dat(exposure="ukb-d-30710_irnt", proxies=FALSE) %>% add_rsq()
+  d <- make_dat(exposure="ukb-d-30710_irnt", proxies=FALSE)
+  if(inherits(d, "response")) skip("Server issues")
+  d <- d %>% add_rsq()
 	expect_true("rsq.outcome" %in% names(d) & "rsq.exposure" %in% names(d))
 })
 
@@ -55,8 +59,8 @@ test_that("effective n", {
 })
 
 test_that("get_population_allele_frequency", {
-  skip("Skip unless you have good access to the API.")  
 	d <- extract_instruments("ieu-a-7")
+	if(inherits(d, "response")) skip("Server issues")
 	d <- add_metadata(d)
 	d$eaf.exposure.controls <- get_population_allele_frequency(
 		af = d$eaf.exposure,
@@ -68,16 +72,16 @@ test_that("get_population_allele_frequency", {
 })
 
 test_that("bbj-a-1", {
-  skip("Skip unless you have good access to the API.")
-  skip_on_ci()
   skip_on_cran()
-  d <- extract_instruments('bbj-a-1') %>% add_metadata() %>% add_rsq()
+  d <- extract_instruments('bbj-a-1')
+  if(inherits(d, "response")) skip("Server issues")
+  d <- d %>% add_metadata() %>% add_rsq()
 	expect_true(all(!is.na(d$rsq.exposure)))
 })
 
 test_that("bsen vs pn", {
-	skip("Skip unless you have good access to the API.")
   d <- extract_instruments("ieu-a-2")
+  if(inherits(d, "response")) skip("Server issues")
 	r1 <- get_r_from_bsen(d$beta.exposure, d$se.exposure, d$samplesize.exposure)
 	r2 <- get_r_from_pn(d$pval.exposure, d$samplesize.exposure)
 	expect_true(cor(abs(r1), r2) > 0.99)
