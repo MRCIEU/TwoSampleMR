4206d2c8cf668ebbe1d40478304afd53a18c2d75
Tom Palmer <remlapmot@hotmail.com>
2024-06-30 15:05:11 +0100

    Split into separate tests and use try()

--- tests/testthat/test_outcomes.R
+++ tests/testthat/test_outcomes.R
@@ -1,36 +1,45 @@
 context("outcome")
 
-test_that("outcomes", {
+skip_if_offline()
+skip_if_offline(host = "api.opengwas.io")
+skip_on_cran()
+skip_on_ci()
 
-  skip_if_offline()
-  skip_if_offline(host = "api.opengwas.io")
-  skip_on_cran()
-  skip_on_ci()
+a <- try(extract_instruments("ieu-a-7"))
+if(inherits(a, "try-error")) skip("Server issues")
 
-	a <- extract_instruments("ieu-a-7")
-	if(inherits(a, "response")) skip("Server issues")
-	b <- extract_outcome_data(a$SNP, "ieu-a-2", proxies=FALSE)
-	if(inherits(b, "response")) skip("Server issues")
+test_that("outcomes 1", {
+	b <- try(extract_outcome_data(a$SNP, "ieu-a-2", proxies=FALSE))
+	if(inherits(b, "try-error")) skip("Server issues")
 	expect_true(nrow(b) < 30 & nrow(b) > 15)
+})
 
-	b <- extract_outcome_data(a$SNP, "ieu-a-2", proxies=TRUE)
-	if(inherits(b, "response")) skip("Server issues")
+test_that("outcomes 2", {
+	b <- try(extract_outcome_data(a$SNP, "ieu-a-2", proxies=TRUE))
+	if(inherits(b, "try-error")) skip("Server issues")
 	expect_true(nrow(b) > 30 & nrow(b) < nrow(a))
+})
 
-	b <- extract_outcome_data(a$SNP, c("ieu-a-2", "a"), proxies=FALSE)
-	if(inherits(b, "response")) skip("Server issues")
+test_that("outcomes 3", {
+	b <- try(extract_outcome_data(a$SNP, c("ieu-a-2", "a"), proxies=FALSE))
+	if(inherits(b, "try-error")) skip("Server issues")
 	expect_true(nrow(b) < 30 & nrow(b) > 15)
+})
 
+test_that("outcomes 4", {
 	b <- extract_outcome_data(a$SNP, c("ieu-a-2", "a"), proxies=TRUE)
-	if(inherits(b, "response")) skip("Server issues")
+	if(inherits(b, "try-error")) skip("Server issues")
 	expect_true(nrow(b) > 30 & nrow(b) < nrow(a))
+})
 
+test_that("outcomes 5", {
 	b <- extract_outcome_data(a$SNP, c("ieu-a-2", "ieu-a-7"), proxies=FALSE)
-	if(inherits(b, "response")) skip("Server issues")
+	if(inherits(b, "try-error")) skip("Server issues")
 	expect_true(nrow(b) > 60)
+})
 
+test_that("outcomes 6", {
 	b <- extract_outcome_data(a$SNP, c("ieu-a-2", "ieu-a-7"), proxies=TRUE)
-	if(inherits(b, "response")) skip("Server issues")
+	if(inherits(b, "try-error")) skip("Server issues")
 	expect_true(nrow(b) > 70)
-
 })
