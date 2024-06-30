347c9ccb1e04000ad735ffb6f79fba2946372c64
Tom Palmer <remlapmot@hotmail.com>
2024-06-30 14:38:06 +0100

    Add try() to catch server non-response

--- tests/testthat/test_add_metadata.r
+++ tests/testthat/test_add_metadata.r
@@ -23,55 +23,65 @@ skip_on_ci()
 load(system.file("extdata", "test_add_metadata.RData", package="TwoSampleMR"))
 
 test_that("exposure data 1", {
-	d1 <- d1 %>% add_metadata()
+	d1 <- try(d1 %>% add_metadata())
+	if (class(d1) == "try-error") skip("Server issues")
 	expect_true("units.exposure" %in% names(d1))
 })
 
 test_that("exposure data 2", {
-	d2 <- d2 %>% add_metadata()
+	d2 <- try(d2 %>% add_metadata())
+	if (class(d2) == "try-error") skip("Server issues")
 	expect_true("units.exposure" %in% names(d2))
 })
 
 test_that("outcome data 1", {
-	d3 <- d3 %>% add_metadata()
+	d3 <- try(d3 %>% add_metadata())
+	if (class(d3) == "try-error") skip("Server issues")
 	expect_true("units.outcome" %in% names(d3))
 })
 
 test_that("outcome data 2", {
-	d4 <- d4 %>% add_metadata()
+	d4 <- try(d4 %>% add_metadata())
+	if (class(d4) == "try-error") skip("Server issues")
 	expect_true("units.outcome" %in% names(d4))
 })
 
 test_that("dat 2", {
-	d5 <- d5 %>% add_metadata()
+	d5 <- try(d5 %>% add_metadata())
+	if (class(d5) == "try-error") skip("Server issues")
 	expect_true("units.outcome" %in% names(d5) & "units.exposure" %in% names(d5))
 })
 
 test_that("no id1", {
 	d6$id.exposure <- "not a real id"
-	d6 <- add_metadata(d6)
+	d6 <- try(add_metadata(d6))
+	if (class(d6) == "try-error") skip("Server issues")
 	expect_true(!"units.exposure" %in% names(d6))
 })
 
 test_that("no id2", {
 	d7$id.outcome <- "not a real id"
-	d7 <- add_metadata(d7)
+	d7 <- try(add_metadata(d7))
+	if (class(d7) == "try-error") skip("Server issues")
 	expect_true(!"units.outcome" %in% names(d7))
 })
 
 test_that("ukb-d", {
-	d8 <- add_metadata(d8)
+	d8 <- try(add_metadata(d8))
+	if (class(d8) == "try-error") skip("Server issues")
 	expect_true("units.outcome" %in% names(d8))
 })
 
 test_that("bbj-a-1", {
-	d9 <- d9 %>% add_metadata()
+	d9 <- try(d9 %>% add_metadata())
+	if (class(d9) == "try-error") skip("Server issues")
 	expect_true("samplesize.exposure" %in% names(d9))
 	expect_true(all(!is.na(d9$samplesize.exposure)))
 })
 
 test_that("ieu-b-109", {
-	d10 <- d10 %>% add_metadata()
+	d10 <- try(d10 %>% add_metadata())
+	if (class(d10) == "try-error") skip("Server issues")
 	expect_true("samplesize.exposure" %in% names(d10))
 	expect_true(all(!is.na(d10$samplesize.exposure)))
 })
