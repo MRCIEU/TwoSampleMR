f387eae6cbeefa13134d5b759d4752d122f58467
Tom Palmer <remlapmot@hotmail.com>
2024-06-30 15:17:38 +0100

    Amend old names

--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -3,7 +3,7 @@ context("plots")
 load(system.file("extdata", "test_commondata.RData", package="TwoSampleMR"))
 
 test_that("scatter plot", {
-	# dat <- make_dat(2,7)
+	# dat <- make_dat("ieu-a-2", "ieu-a-7")
 	m <- mr(dat, method_list="mr_ivw")
 	p <- mr_scatter_plot(m, dat)
 	expect_true(is.list(p))
