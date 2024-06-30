605f0b4c89b1e3aeff96caa3c6839e6778a4ea49
Tom Palmer <remlapmot@hotmail.com>
2024-06-30 15:12:50 +0100

    Delete blank lines

--- tests/testthat/test_steiger.R
+++ tests/testthat/test_steiger.R
@@ -3,19 +3,16 @@ context("steiger")
 # w <- make_dat(2, 7)
 load(system.file("extdata", "test_commondata.RData", package="TwoSampleMR"))
 
-
 test_that("directionality", {
 	o <- directionality_test(dat)
 	expect_true(nrow(o) == 1)
 })
-
 
 test_that("directionality cc", {
 	dat$r.outcome <- get_r_from_lor(dat$beta.outcome, dat$eaf.outcome, dat$samplesize.outcome/2, dat$samplesize.outcome/2, 0.1)
 	o <- directionality_test(dat)
 	expect_true(nrow(o) == 1)
 })
-
 
 test_that("steiger filtering", {
 	expect_warning(dat <- steiger_filtering(dat))
