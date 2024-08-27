599c071aa788e1268cad29a3892296d0ec94bc23
Tom Palmer <remlapmot@hotmail.com>
2024-08-27 15:17:19 +0100

    Whitespace

--- R/make_dat.R
+++ R/make_dat.R
@@ -1,7 +1,7 @@
 #' Convenient function to create a harmonised dataset
 #'
 #' Convenient function to create a harmonised dataset.
-#' 
+#'
 #' @param exposures The default is `c("ieu-a-2", "ieu-a-301")` (BMI and LDL).
 #' @param outcomes The default is `c("ieu-a-7", "ieu-a-1001")` (CHD and EDU).
 #' @param proxies Look for proxies? Default = `TRUE`
