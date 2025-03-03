a0daa944c31121110248282453b8c206c76425fa
Tom Palmer <remlapmot@hotmail.com>
2025-03-03 10:18:38 +0000

    Add test for mr() with default options

---
+++ tests/testthat/test_mr.R
@@ -0,0 +1,15 @@
+# Test mr()
+
+# dat <- make_dat("ieu-a-2", "ieu-a-7")
+load(system.file("extdata", "test_commondata.RData", package="TwoSampleMR"))
+
+test_that("Test mr(): MR Egger, Weighted median, Inverse variance weighted, Simple mode, Weighted mode", {
+  res <- mr(dat)
+  expect_equal(nrow(res), 5L)
+  expect_equal(ncol(res), 9L)
+  expect_equal(res[1, "b"], 0.5025, tolerance = 1e-4)
+  expect_equal(res[2, "b"], 0.3870, tolerance = 1e-4)
+  expect_equal(res[3, "b"], 0.4459, tolerance = 1e-4)
+  expect_equal(res[4, "b"], 0.3402, tolerance = 1e-4)
+  expect_equal(res[5, "b"], 0.3791, tolerance = 1e-4)
+})
