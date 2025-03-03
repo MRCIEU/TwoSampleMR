33fe25eae94d1bfb4083f89843498a61c9e15815
Tom Palmer <remlapmot@hotmail.com>
2025-03-03 12:09:46 +0000

    Amend tolerance

--- tests/testthat/test_mr.R
+++ tests/testthat/test_mr.R
@@ -7,18 +7,18 @@ test_that("Test mr(): MR Egger, Weighted median, Inverse variance weighted, Simp
   res <- mr(dat)
   expect_equal(nrow(res), 5L)
   expect_equal(ncol(res), 9L)
-  expect_equal(res[1, "b"], 0.5025, tolerance = 1e-4)
-  expect_equal(res[2, "b"], 0.3870, tolerance = 1e-4)
-  expect_equal(res[3, "b"], 0.4459, tolerance = 1e-4)
-  expect_equal(res[4, "b"], 0.3402, tolerance = 1e-4)
-  expect_equal(res[5, "b"], 0.3791, tolerance = 1e-4)
+  expect_equal(res[1, "b"], 0.5025, tolerance = 1e-3)
+  expect_equal(res[2, "b"], 0.3870, tolerance = 1e-3)
+  expect_equal(res[3, "b"], 0.4459, tolerance = 1e-3)
+  expect_equal(res[4, "b"], 0.3402, tolerance = 1e-3)
+  expect_equal(res[5, "b"], 0.3791, tolerance = 1e-3)
 })
 
 test_that("mr.raps", {
   res2 <- suppressWarnings(mr(dat, method_list = "mr_raps"))
   expect_equal(nrow(res2), 1L)
   expect_equal(ncol(res2), 9L)
-  expect_equal(res2[1, "b"], 0.4647, tolerance = 1e-4)
+  expect_equal(res2[1, "b"], 0.4647, tolerance = 1e-3)
 })
 
 test_that("mr.raps over.dispersion option", {
@@ -27,7 +27,7 @@ test_that("mr.raps over.dispersion option", {
   res3 <- suppressWarnings(mr(dat, method_list = "mr_raps", parameters = params))
   expect_equal(nrow(res3), 1L)
   expect_equal(ncol(res3), 9L)
-  expect_equal(res3[1, "b"], 0.4682, tolerance = 1e-4)
+  expect_equal(res3[1, "b"], 0.4682, tolerance = 1e-3)
 })
 
 test_that("mr.raps loss.function option", {
@@ -36,7 +36,7 @@ test_that("mr.raps loss.function option", {
   res4 <- suppressWarnings(mr(dat, method_list = "mr_raps", parameters = params))
   expect_equal(nrow(res4), 1L)
   expect_equal(ncol(res4), 9L)
-  expect_equal(res4[1, "b"], 0.4788, tolerance = 1e-4)
+  expect_equal(res4[1, "b"], 0.4788, tolerance = 1e-3)
 })
 
 test_that("mr.raps shrinkage option", {
@@ -45,5 +45,5 @@ test_that("mr.raps shrinkage option", {
   res5 <- suppressWarnings(mr(dat, method_list = "mr_raps", parameters = params))
   expect_equal(nrow(res5), 1L)
   expect_equal(ncol(res5), 9L)
-  expect_equal(res5[1, "b"], 0.4647, tolerance = 1e-4)
+  expect_equal(res5[1, "b"], 0.4647, tolerance = 1e-3)
 })
