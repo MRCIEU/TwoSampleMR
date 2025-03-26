aacdd2fef1f7737b4217f5eacfc35a2a327bb076
Tom Palmer <remlapmot@hotmail.com>
2025-03-26 07:56:20 +0000

    Update mr-grip.R

--- R/mr-grip.R
+++ R/mr-grip.R
@@ -1,6 +1,6 @@
 #' MR-GRIP: a modified MR-Egger model with the Genotype Recoding Invariant Property
 #'
-#' This implements the modified MR-Egger model with the Genotype Recoding Invariant Property (MR-GRIP).
+#' This implements the modified MR-Egger model with the Genotype Recoding Invariant Property (MR-GRIP) due to Dudbridge and Bowden et al. (2025).
 #' It is well known that the results of MR-Egger are sensitive to which alleles are designated as the effect alleles.
 #' A pragmatic convention is to orient all SNPs to have positive effects on the exposure, which has some advantages in interpretation but also brings some philosophical limitations.
 #' The MR-GRIP model is a modification to the MR-Egger model in which each term is multiplied by the genotype-phenotype associations.
