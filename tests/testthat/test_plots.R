a0fd0383fad58676126adfd06a71b3747e7a5a39
Tom Palmer <remlapmot@hotmail.com>
2025-06-05 08:57:00 +0100

    Add scatter plot test for default set of estimates

--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -9,3 +9,11 @@ test_that("MR scatter plot for mr_ivw", {
 	expect_true(is.list(p))
 	expect_true(length(p) == 1L)
 })
+
+test_that("Scatter plot for default set of estimates", {
+  # dat <- make_dat("ieu-a-2", "ieu-a-7")
+  m2 <- mr(dat)
+  p2 <- mr_scatter_plot(m2, dat)
+  expect_true(is.list(p2))
+  expect_true(length(p2) == 1L)
+})
