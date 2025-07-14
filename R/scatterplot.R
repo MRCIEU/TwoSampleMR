3d776973e2722d704030be9b453a2d5e0640c1b8
Tom Palmer <remlapmot@hotmail.com>
2025-07-14 05:47:05 +0100

    Fix geom_errorbarh arg height to width

--- R/scatterplot.R
+++ R/scatterplot.R
@@ -55,6 +55,7 @@ mr_scatter_plot <- function(mr_results, dat) {
       mrres$a[mrres$method == "MR GRIP"] <- stats::lm(d$beta.outcome - temp$b*d$beta.exposure ~ 1, weights = 1/d$se.outcome^2)$coef
     }
 
+    if (packageVersion("ggplot2") <= "3.5.2") {
     ggplot2::ggplot(
       data = d,
       ggplot2::aes(x = beta.exposure, y = beta.outcome)
@@ -104,6 +105,57 @@ mr_scatter_plot <- function(mr_results, dat) {
       ) +
       ggplot2::theme(legend.position = "top", legend.direction = "vertical") +
       ggplot2::guides(colour = ggplot2::guide_legend(ncol = 2))
+    } else {
+      ggplot2::ggplot(
+        data = d,
+        ggplot2::aes(x = beta.exposure, y = beta.outcome)
+      ) +
+        ggplot2::geom_errorbar(
+          ggplot2::aes(
+            ymin = beta.outcome - se.outcome,
+            ymax = beta.outcome + se.outcome
+          ),
+          colour = "grey",
+          width = 0
+        ) +
+        ggplot2::geom_errorbarh(
+          ggplot2::aes(
+            xmin = beta.exposure - se.exposure,
+            xmax = beta.exposure + se.exposure
+          ),
+          colour = "grey",
+          width = 0
+        ) +
+        ggplot2::geom_point() +
+        ggplot2::geom_abline(
+          data = mrres,
+          ggplot2::aes(intercept = a, slope = b, colour = method),
+          show.legend = TRUE
+        ) +
+        ggplot2::scale_colour_manual(
+          values = c(
+            "#a6cee3",
+            "#1f78b4",
+            "#b2df8a",
+            "#33a02c",
+            "#fb9a99",
+            "#e31a1c",
+            "#fdbf6f",
+            "#ff7f00",
+            "#cab2d6",
+            "#6a3d9a",
+            "#ffff99",
+            "#b15928"
+          )
+        ) +
+        ggplot2::labs(
+          colour = "MR Estimate",
+          x = paste("SNP effect on", d$exposure[1]),
+          y = paste("SNP effect on", d$outcome[1])
+        ) +
+        ggplot2::theme(legend.position = "top", legend.direction = "vertical") +
+        ggplot2::guides(colour = ggplot2::guide_legend(ncol = 2))
+    }
   })
   mrres
 }
