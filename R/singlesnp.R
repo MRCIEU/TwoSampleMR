8655f02b02ac4f64f7a076fddd94bd6b005ead3a
Tom Palmer <remlapmot@hotmail.com>
2025-07-14 08:58:21 +0100

    Fix height to width in mr_forest_plot()

--- R/singlesnp.R
+++ R/singlesnp.R
@@ -111,6 +111,7 @@ mr_forest_plot <- function(singlesnp_results, exponentiate=FALSE)
 			xint <- 1
 		}
 
+		if (packageVersion("ggplot2") <= "3.5.2") {
 		ggplot2::ggplot(d, ggplot2::aes(y=SNP, x=b)) +
 		ggplot2::geom_vline(xintercept=xint, linetype="dotted") +
 		# ggplot2::geom_errorbarh(ggplot2::aes(xmin=pmax(lo, min(d$b, na.rm=T)), xmax=pmin(up, max(d$b, na.rm=T)), size=as.factor(tot), colour=as.factor(tot)), height=0) +
@@ -126,6 +127,23 @@ mr_forest_plot <- function(singlesnp_results, exponentiate=FALSE)
 			axis.ticks.y=ggplot2::element_line(linewidth=0),
 			axis.title.x=ggplot2::element_text(size=8)) +
 		ggplot2::labs(y="", x=paste0("MR effect size for\n'", d$exposure[1], "' on '", d$outcome[1], "'"))
+		} else {
+		  ggplot2::ggplot(d, ggplot2::aes(y=SNP, x=b)) +
+		    ggplot2::geom_vline(xintercept=xint, linetype="dotted") +
+		    # ggplot2::geom_errorbarh(ggplot2::aes(xmin=pmax(lo, min(d$b, na.rm=T)), xmax=pmin(up, max(d$b, na.rm=T)), size=as.factor(tot), colour=as.factor(tot)), height=0) +
+		    ggplot2::geom_errorbarh(ggplot2::aes(xmin=lo, xmax=up, linewidth=as.factor(tot), colour=as.factor(tot)), width=0) +
+		    ggplot2::geom_point(ggplot2::aes(colour=as.factor(tot))) +
+		    ggplot2::geom_hline(ggplot2::aes(yintercept = which(levels(SNP) %in% "")), colour="grey") +
+		    ggplot2::scale_colour_manual(values=c("black", "red")) +
+		    ggplot2::scale_linewidth_manual(values=c(0.3, 1)) +
+		    # xlim(c(min(c(0, d$b), na.rm=T), max(c(0, d$b), na.rm=T))) +
+		    ggplot2::theme(
+		      legend.position="none",
+		      axis.text.y=ggplot2::element_text(size=8),
+		      axis.ticks.y=ggplot2::element_line(linewidth=0),
+		      axis.title.x=ggplot2::element_text(size=8)) +
+		    ggplot2::labs(y="", x=paste0("MR effect size for\n'", d$exposure[1], "' on '", d$outcome[1], "'"))
+		}
 	})
 	res
 }
