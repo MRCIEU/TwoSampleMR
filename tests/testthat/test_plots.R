bfeb0324066f41852299499c9834b6a911a072e7
Tom Palmer <remlapmot@hotmail.com>
2025-07-14 10:35:43 +0100

    Add expect_no_error() calls to existing plot tests

--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -13,21 +13,21 @@ test_that("MR scatter plot for mr_ivw", {
 test_that("Scatter plot for default set of estimates", {
   # dat <- make_dat("ieu-a-2", "ieu-a-7")
   m2 <- mr(dat)
-  p2 <- mr_scatter_plot(m2, dat)
+  expect_no_error(p2 <- mr_scatter_plot(m2, dat))
   expect_true(is.list(p2))
   expect_true(length(p2) == 1L)
 })
 
 test_that("Scatter plot for mr_grip", {
   m3 <- mr(dat, method_list = "mr_grip")
-  p3 <- mr_scatter_plot(m3, dat)
+  expect_no_error(p3 <- mr_scatter_plot(m3, dat))
   expect_true(is.list(p3))
   expect_true(length(p3) == 1L)
 })
 
 test_that("A second scatter plot for mr_grip", {
   m4 <- mr(dat2, method_list = "mr_grip")
-  p4 <- mr_scatter_plot(m4, dat2)
+  expect_no_error(p4 <- mr_scatter_plot(m4, dat2))
   expect_true(is.list(p4))
   expect_true(length(p4) == 4L)
 })
